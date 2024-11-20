89349b8afdd9cb0ace7e6ee38d0885c8c3c2a529
RosieChen626 <rosie.chenyy@shopee.com>
2024-11-20 22:13:30 +0800

    Update login.js

--- json/login.js
+++ json/login.js
@@ -4,32 +4,27 @@ btn.addEventListener('click', ()=>{
     const loading = document.querySelector(".loading");
     loading.classList.add('transfrom');
     const searchTerm = document.getElementById('phoneNum').value;
-    let run = setInterval(()=>{
+    for(i=0; i<=500000; i++){
       setTimeout(()=>{
         const data = localStorage.getItem("data");
         if(data){
-          clearInterval(run);
           window.location.href = `./mainpage.html?phone=${searchTerm}`;
           return
         }
       },500)
-    })
+    }
 });
 
 function fetchData() {
     fetch(
       "https://script.google.com/macros/s/AKfycbz1MfB3vVV3hiXH7D-pwdA5AiHw8rFHBghmHW5LyG0_t6wpQXIawpE7-hCJfkmGug5c3A/exec"
     )
-      .then((response) => {
-          if(response.status==200){
-          alert('succesfull');
-          response.json()})
-      .then((data) => localStorage.setItem('data',JSON.stringify(data)))
+      .then((response) => response.json())
+      .then((data) => localStorage.setItem('data',"1"))
       .catch((error) => {
         console.error("Error fetching data:", error);
         alert("Error in fetching data. Please try again.");
       });
- }
   }
 
   window.onload = function () {
