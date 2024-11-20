8e61ba31886f9568d6bf8f37fd9456b9950c0439
RosieChen626 <rosie.chenyy@shopee.com>
2024-11-20 14:33:13 +0800

    Update login.js

--- json/login.js
+++ json/login.js
@@ -1,15 +1,36 @@
 const btn = document.querySelector(".CA");
 
 btn.addEventListener('click', ()=>{
+    const loading = document.querySelector(".loading");
+    loading.classList.add('transfrom');
     const searchTerm = document.getElementById('phoneNum').value;
-    console.log(searchTerm);
-    window.location.href = `./mainpage.html?phone=${searchTerm}`
+    for(i=0; i<=500000; i++){
+      setTimeout(()=>{
+        const data = localStorage.getItem("data");
+        if(data){
+          window.location.href = `./mainpage.html?phone=${searchTerm}`;
+          return
+        }
+      },500)
+    }
 });
 
-// function getClickID(clickID) {
-//     window.location.href = `./mainpage.html?phone=${searchTerm}`
-// }
+function fetchData() {
+    fetch(
+      "https://script.google.com/macros/s/AKfycbz1MfB3vVV3hiXH7D-pwdA5AiHw8rFHBghmHW5LyG0_t6wpQXIawpE7-hCJfkmGug5c3A/exec"
+    )
+      .then((response) => response.json())
+      .then((data) => localStorage.setItem('data',JSON.stringify(data)))
+      .catch((error) => {
+        console.error("Error fetching data:", error);
+        alert("Error in fetching data. Please try again.");
+      });
+  }
 
+  window.onload = function () {
+    localStorage.removeItem('data');
+    fetchData();
+  }
 
 
 
