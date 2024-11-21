efaffbdb0dd8d17ecf6bb9017e1857c845df0534
RosieChen626 <rosie.chenyy@shopee.com>
2024-11-21 12:32:20 +0800

    Update login.js

--- json/login.js
+++ json/login.js
@@ -3,16 +3,18 @@ const btn = document.querySelector(".CA");
 btn.addEventListener('click', ()=>{
     const loading = document.querySelector(".loading");
     loading.classList.add('transfrom');
-    const searchTerm = document.getElementById('phoneNum').value;
-    for(i=0; i<=500000; i++){
+    // const searchTerm = document.getElementById('phoneNum').value;
+    let submit = setInterval(()=>{
       setTimeout(()=>{
         const data = localStorage.getItem("data");
         if(data){
+          clearInterval(submit);
+          const searchTerm = document.getElementById('phoneNum').value;
           window.location.href = `./mainpage.html?phone=${searchTerm}`;
           return
         }
       },500)
-    }
+    })
 });
 
 function fetchData() {
@@ -20,7 +22,9 @@ function fetchData() {
         "https://script.google.com/macros/s/AKfycbz1MfB3vVV3hiXH7D-pwdA5AiHw8rFHBghmHW5LyG0_t6wpQXIawpE7-hCJfkmGug5c3A/exec"
     )
       .then((response) => response.json())
-      .then((data) => localStorage.setItem('data',JSON.stringify(data)))
+      .then((data) => {
+        filterData(data);
+      })
       .catch((error) => {
         console.error("Error fetching data:", error);
         alert("Error in fetching data. Please try again.");
@@ -36,6 +40,27 @@ const goreport = document.querySelector(".goreport");
 goreport.addEventListener('click', ()=>{
     window.location.href = `./report.html`;
 })
+
+function filterData(data){
+  let run = setInterval(()=>{
+    const searchtype = document.getElementById('searchtype').value;
+    const searchTerm = document.getElementById('phoneNum').value;
+    if(searchtype==="WINDOWS/Android"){
+      clearInterval(run);
+      localStorage.setItem('data',JSON.stringify(data))
+      }
+    else if(searchtype==="IOS"){
+        if(searchTerm.length===10){
+          clearInterval(run);
+          filteredresult = data.filter((da) =>
+            da.rider_phone_num.startsWith(searchTerm));
+          console.log(filteredresult);
+          localStorage.setItem('data',JSON.stringify(filteredresult))
+        }
+      }
+    console.log(searchtype);
+  })
+}
 
 
 
